ff1394ba882c48728fa01e104d2c388e24666ada
Gabriel Abarca <gababarca@hotmail.com>
2020-06-20 03:03:24 -0600

    [Add] Console command goroutine

--- src/main.go
+++ src/main.go
@@ -1,9 +1,33 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"sync"
 )
 
 func main() {
-	fmt.Println("Hello World")
+	fmt.Println("Server Starting...")
+	var wg sync.WaitGroup
+	wg.Add(1)
+	go consoleThread()
+	wg.Wait()
+}
+
+func consoleThread() {
+	var line string
+	running := true
+	reader := bufio.NewReader(os.Stdin)
+
+	for running {
+		line, _ = reader.ReadString('\n')
+
+		if len(line) == 0 {
+			running = false
+			break
+		} else {
+
+		}
+	}
 }
